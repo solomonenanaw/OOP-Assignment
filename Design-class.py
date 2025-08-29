73dbc243159a5cb880f35093e9ac859cf03603d1
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2025-08-29 09:59:25 +0300

    Design-class.py

--- Design-class.py
+++ Design-class.py
@@ -1,53 +1,52 @@
 # Base class
-class Book:
-    def __init__(self, title, author, pages, genre):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.genre = genre
-        self.__is_checked_out = False  # Encapsulation: private attribute
-
-    def read(self):
-        return f"You start reading '{self.title}' by {self.author}."
-
-    def check_out(self):
-        if not self.__is_checked_out:
-            self.__is_checked_out = True
-            return f"'{self.title}' has been checked out."
-        else:
-            return f"'{self.title}' is already checked out."
-
-    def return_book(self):
-        if self.__is_checked_out:
-            self.__is_checked_out = False
-            return f"'{self.title}' has been returned."
-        else:
-            return f"'{self.title}' was not checked out."
-
-    def is_checked_out(self):
-        return self.__is_checked_out
+class Superhero:
+    def __init__(self, name, secret_identity, powers):
+        self.name = name
+        self.secret_identity = secret_identity
+        self.powers = powers
+        self.__is_fighting = False  # Encapsulation: private attribute
+    
+    def introduce(self):
+        return f"I am {self.name}!"
+    
+    def use_power(self):
+        return f"{self.name} uses {self.powers}!"
+    
+    def start_fight(self):
+        if not self.__is_fighting:
+            self.__is_fighting = True
+            return f"{self.name} is now fighting crime!"
+        return f"{self.name} is already fighting!"
+    
+    def end_fight(self):
+        if self.__is_fighting:
+            self.__is_fighting = False
+            return f"{self.name} has stopped fighting."
+        return f"{self.name} wasn't fighting."
+    
+    def is_fighting(self):
+        return self.__is_fighting
 
 # Subclass
-class EBook(Book):
-    def __init__(self, title, author, pages, genre, file_size_mb):
-        super().__init__(title, author, pages, genre)
-        self.file_size_mb = file_size_mb
-
+class FlyingSuperhero(Superhero):
+    def __init__(self, name, secret_identity, powers, max_speed):
+        super().__init__(name, secret_identity, powers)
+        self.max_speed = max_speed
+    
     # Polymorphism: override method
-    def read(self):
-        return f"You open the ebook '{self.title}' on your device."
-
-    def download(self):
-        return f"'{self.title}' ({self.file_size_mb}MB) is downloading..."
+    def use_power(self):
+        return f"{self.name} soars through the sky at {self.max_speed} mph!"
+    
+    def fly(self):
+        return f"{self.name} takes to the skies!"
 
 # Example usage
-physical_book = Book("1984", "George Orwell", 328, "Dystopian")
-ebook = EBook("Digital Fortress", "Dan Brown", 356, "Thriller", 2.5)
+hero = Superhero("Shadow", "Alex Morgan", "invisibility")
+flying_hero = FlyingSuperhero("Skyhawk", "Jamie Chen", "flight and super strength", 500)
 
-print(physical_book.read())
-print(physical_book.check_out())
-print(physical_book.return_book())
-
-print(ebook.read())           # Polymorphic behavior
-print(ebook.download())
-print(ebook.check_out())      # Inherited method
+print(hero.introduce())
+print(hero.start_fight())
+print(hero.end_fight())
+print(flying_hero.use_power())  # Polymorphic behavior
+print(flying_hero.fly())
+print(flying_hero.start_fight())  # Inherited method
